321e453feb6c6e65680cb2ee9e8b6747f7d39755
Soomint <ttnne1@gmail.com>
2023-07-30 19:43:15 +0900

    closure 예시 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import Image from 'next/image'
+'use client'
+
 import styles from './page.module.css'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log('start loading');
+  }, [])
+
+  const addCount = () => {
+    let count = 0;
+
+    const addCount = () => {
+      count++;
+      console.log('count : ' + count);
+    }
+
+    return addCount;
+  }
+
+  const count = addCount();
+
   return (
     <main className={styles.main}>
       <h4>hello</h4>
+      <button onClick={() => count()}>add counter</button>
     </main>
   )
 }
